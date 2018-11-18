41b104642fde8c763f310c2be5399d3dbee80b0a
pedrovidal <peu_vs@yahoo.com.br>
2018-11-18 16:07:41 -0300

    Remove unnecessary code from lista3/c_XMEN.cpp

--- lista3/c_XMEN.cpp
+++ lista3/c_XMEN.cpp
@@ -4,31 +4,34 @@ using namespace std;
 #define int long long
 #define INF LLONG_MAX
 #define _INF LLONG_MIN
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
 #define endl '\n'
+#define ta a.size() + 1
+#define tb b.size() + 1
+#define DESYNC ios::sync_with_stdio(false)
 
-int calc_lis(vector<int> & vet, int ini, int fim, vector<int> & best){
-	vector<int> lis(fim + 1, INF);
+int calc_lis(vi & vet, int n){
+	vi lis(n + 1, INF);
 	lis[0] = 0;
-	lis[1] = vet[ini];
-	int res = 1;
-	for (int i = ini; i < fim; i++){
-		auto pos = lower_bound(lis.begin(), lis.end(), vet[i]);
-		if (pos == lis.end()) continue;
-		lis[pos - lis.begin()] = vet[i];
-		best[i] = pos - lis.begin();
-		res = max(res, best[i]);
+	int res = 0;
+	for (int i = 0; i < n; i++){
+		int pos = lower_bound(lis.begin(), lis.end(), vet[i])  - lis.begin();
+		lis[pos] = vet[i];
+		res = max(res, pos);
 	}
 	return res;
 }
 
 signed main(){
+	DESYNC;
 	int t;
 	cin >> t;
 	for (int i = 0; i < t; i++){
 		int n;
 		cin >> n;
 		vector<int> vet(n);
-		vector<int> best(n, 0);
 		map<int, int> pos_id;
 		for (int i = 0; i < n; i++){
 			cin >> vet[i];
@@ -38,7 +41,6 @@ signed main(){
 			cin >> vet[i];
 			vet[i] = pos_id[vet[i]];
 		}
-		int res = calc_lis(vet, 0, n, best);
-		cout << res << endl;
+		cout << calc_lis(vet, n) << endl;
 	}
 }
--- template.cpp
+++ template.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #define int long long
 #define INF LLONG_MAX
 #define _INF LLONG_MIN
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
 #define endl '\n'
 
 signed main(){
