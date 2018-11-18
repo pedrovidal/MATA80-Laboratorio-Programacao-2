eaddc14e259812bddbee38efaa1f597666eb4a36
pedrovidal <peu_vs@yahoo.com.br>
2018-11-18 16:10:37 -0300

    Add defines

---
+++ lib/codes/defines.cpp
@@ -0,0 +1,9 @@
+#define int long long
+#define INF LLONG_MAX
+#define _INF LLONG_MIN
+#define ta a.size() + 1
+#define tb b.size() + 1
+#define vi vector<int>
+#define vvi vector<vi>
+#define pii pair<int, int>
+#define endl '\n'
